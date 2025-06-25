15ab9acc4f5b87207f0bebbaf703e8ee593469aa
kimbora <k.bora9322@gmail.com>
2025-06-26 00:45:37 +0900

    함수로 실행

--- 02Website/js/notice.js
+++ 02Website/js/notice.js
@@ -2,28 +2,37 @@ import { noticeData } from "./noticeData.js";
 
 export const NoticeList = () => {
   const tbody = document.querySelector("table.notice tbody");
-  const boardInfo = document.querySelector(".board-info span");
 
   if (!tbody) {
     return;
   }
+  // 개시물 데이터 바인딩
+  const boardData = () => {
+    if (noticeData.length === 0) {
+      tbody.innerHTML = `<tr><td colspan="5">등록된 게시물이 없습니다.</td></tr>`;
+      return;
+    }
 
-  boardInfo.textContent = `${noticeData.length.toLocaleString()} 개`;
+    tbody.innerHTML = noticeData
+      .map(({ no, title, writer, date, review }) => {
+        return `<tr>
+            <td>${no}</td>
+            <td>${title}</td>
+            <td>${writer}</td>
+            <td>${date}</td>
+            <td>${review}</td>
+          </tr>`;
+      })
+      .join("");
+  };
 
-  if (noticeData.length === 0) {
-    tbody.innerHTML = `<tr><td colspan="5">등록된 게시물이 없습니다.</td></tr>`;
-    return;
-  }
+  // 개시물 갯수
+  const boardLength = () => {
+    const boardInfo = document.querySelector(".board-info span");
+    boardInfo.textContent = `${noticeData.length.toLocaleString()} 개`;
+  };
 
-  tbody.innerHTML = noticeData
-    .map(({ no, title, writer, date, review }) => {
-      return `<tr>
-        <td>${no}</td>
-        <td>${title}</td>
-        <td>${writer}</td>
-        <td>${date}</td>
-        <td>${review}</td>
-      </tr>`;
-    })
-    .join("");
+  //함수 실행
+  boardData();
+  boardLength();
 };
